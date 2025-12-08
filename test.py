0ed10cd0a87d0a0909530e3bb5f6092cda850915
Polina <polina.ivantaeva@edu.univ-eiffel.fr>
2025-12-08 15:20:17 +0100

    j'ai commence une fonction nouvelle

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from fltk import *
 import math
 import main2
 
+
 def open_shapefile():
     path = "departements-20180101-shp/departements-20180101.shp"
     sh_file = shapefile.Reader(path)
@@ -11,27 +12,29 @@ def open_shapefile():
 
 sh_file = open_shapefile()
 
+
 def trie_sh():
     resultat = []
     records = sh_file.records()
     shapes = sh_file.shapes()
 
     for rec, shp in zip(records, shapes):
-        code_dep = rec[0]  
+        code_dep = rec[0]
 
-        if code_dep in ['971', '972', '973', '974', '976']:  
+        if code_dep in ['971', '972', '973', '974', '976']:
             continue
         resultat.append(shp)
 
     return resultat, records
 
 
-
 xmin, ymin, xmax, ymax = sh_file.bbox
 all_shapes, records = trie_sh()
 
+
 class GeoScale:
-    def __init__(self, xmin, ymin, xmax, ymax, largeur, hauteur, aspect=True):
+    def __init__(self, xmin, ymin, xmax, ymax, largeur, hauteur, aspect=True, database = None):
+
         self.xmin = xmin
         self.ymin = ymin
         self.xmax = xmax
@@ -39,10 +42,11 @@ class GeoScale:
         self.largeur = largeur
         self.hauteur = hauteur
         self.aspect = aspect
+        self.database = database if database is not None else {}
 
 
         dx = xmax - xmin
-        dy = ymax - ymin #Диапазон между макс и мин координатoi
+        dy = ymax - ymin  # Диапазон между макс и мин координатoi
 
         sx = self.largeur / dx
         sy = self.hauteur / dy
@@ -51,27 +55,29 @@ class GeoScale:
         map_larg = dx * self.scale
         map_haut = dy * self.scale
         self.offset_x = (largeur - map_larg) / 2
-        self.offset_y = (hauteur - map_haut)   #tabs for having a map in the middle
-
+        self.offset_y = (hauteur - map_haut)  # tabs for having a map in the middle
 
     # ça marche mais c'est un peu brouillon ?
     def from_geo_to_pix(self, lon, lat):
         # lat_rad = math.radians(lat)
         x = (lon - self.xmin) * self.scale + self.offset_x
-        y = (self.ymax - math.degrees(math.log(math.tan(math.pi / 4 + math.radians(lat) / 2)))) * self.scale + self.offset_y
-        return x, y  
+        y = (self.ymax - math.degrees(
+            math.log(math.tan(math.pi / 4 + math.radians(lat) / 2)))) * self.scale + self.offset_y
+        return x, y
+
 
 def temp_par_departament():
     data = main2.trie()  # liste de liste
     temp_departament = {}
     for i in range(1, len(data)):
-        if data[i][5] == '' :
+        if data[i][5] == '':
             continue
         else:
             key = data[i][1]
             temp = float(data[i][5])
             temp_departament[key] = temp
     return temp_departament
+
 
 def temp_to_color(temp, tmin, tmax):
     if tmax == tmin:
@@ -83,62 +89,66 @@ def temp_to_color(temp, tmin, tmax):
     b = int(255 - (255 - 0) * norm)
     return f'#{r:02x}{g:02x}{b:02x}'
 
+
 temp_departament = temp_par_departament()
 tmin = min(temp_departament.values())
 tmax = max(temp_departament.values())
-scale = GeoScale(xmin=xmin, ymin=ymin, xmax=xmax, ymax=ymax, largeur=800, hauteur=800)
+scale = GeoScale(xmin=xmin, ymin=ymin, xmax=xmax, ymax=ymax, largeur=800, hauteur=800, database= {})
 cree_fenetre(scale.largeur, scale.hauteur)
 x, y = 770, 0
 temperature = -30
-
 
 for i in range(-30, 40, 2):
     color = temp_to_color(i, -30, 40)
     if len(color) == 7:
         final_color = color
-    #print(color, len(color))
-    rectangle(x,y, x + 30, y + 12, remplissage = final_color)
+    # print(color, len(color))
+    rectangle(x, y, x + 30, y + 12, remplissage=final_color)
     if int(i) % 10 == 0:
-        texte(x-30, y, f'{int(i)} -', couleur = 'black', taille = 10)
+        texte(x - 30, y, f'{int(i)} -', couleur='black', taille=10)
 
     y += 12
 
 
-
-
-
-def draw_shape(shape, record):
+def draw_shape(shape, record, numero_depart):
     depart_code = record[0]
 
     if depart_code in temp_departament.keys():
         temp = temp_departament[depart_code]
         depart_color = temp_to_color(temp, tmin, tmax)
-        #print(depart_code, temp, depart_color)
+        # print(depart_code, temp, depart_color)
     else:
         depart_color = None
-    pnts = shape.points #tous les points de polygone
-    prts = list(shape.parts) + [len(pnts)]  #toutes les parties de polygone, on ajoute len pour qu'on puisse
-                                                # obtenir le dernier point, pour qu'on puisse savoir
-                                                  # quand il faut finit de faire dernier polyg
-    for i in range(len(prts)-1):
+    pnts = shape.points  # tous les points de polygone
+    prts = list(shape.parts) + [len(pnts)]  # toutes les parties de polygone, on ajoute len pour qu'on puisse
+    # obtenir le dernier point, pour qu'on puisse savoir
+    # quand il faut finit de faire dernier polyg
+    polygone_pixels = []
+    for i in range(len(prts) - 1):
 
-        segment = pnts[prts[i]:prts[i+1]] #partage par des segments (on ecrit les points de ca ([0:150]))
+        segment = pnts[prts[i]:prts[i + 1]]  # partage par des segments (on ecrit les points de ca ([0:150]))
         segment_pixels = []
         for lon, lat in segment:
             coords = scale.from_geo_to_pix(lon, lat)
             segment_pixels.append(coords)
-        #print(depart_color)
-        polygone(segment_pixels, couleur = "black", remplissage = depart_color)
+            polygone_pixels.append(coords)
+
+        polygone(segment_pixels, couleur="black", remplissage=depart_color)
+    xmin_pix = min([p[0] for p in polygone_pixels])
+    xmax_pix = max([p[0] for p in polygone_pixels])
+    ymin_pix = min([p[1] for p in polygone_pixels])
+    ymax_pix = max([p[1] for p in polygone_pixels])
+    scale.database[numero_depart] = (xmin_pix, ymin_pix, xmax_pix,  ymax_pix)
 
 
-for shape, record in zip(all_shapes, records):
-    draw_shape(shape, record)
 
 
+for numero_depart, (shape, record) in enumerate(zip(all_shapes, records)):
+    draw_shape(shape, record, numero_depart)
 
-def boutons ():
+
+def boutons():
     ligne(0)
-
 
 
 # Permet de se déplacer sur la carte avec les touches directionnelles
@@ -154,18 +164,25 @@ def se_deplacer(speed=10):
         dy -= speed
     if dx != 0 or dy != 0:
         deplace("all", dx, dy)
-        
+
+
 while True:
     se_deplacer()
     mise_a_jour()
-    
+
     ev = donne_ev()
     if type_ev(ev) == 'Quitte':
-            break
+        break
     if type_ev(ev) == "ClicGauche":
         x = abscisse(ev)
         y = ordonnee(ev)
         """je suis en train d'essayer de faire un affichage de chaque polygone"""
+        for numero_depart, (xmin_pix, ymin_pix, xmax_pix, ymax_pix) in scale.database.items():
+            if xmin_pix <= x <= xmax_pix and ymin_pix <= y <= ymax_pix:
+                clicked_polygone = numero_depart
+                break
+        if clicked_polygone:
+            print(f'vous avez clique sur Polygone {clicked_polygone}')
 
 
 ferme_fenetre()
